168dcbe4a08c96c1e524533358b846e267beee9d
salsaa-334 <salsabilhaque13@gmail.com>
2026-02-19 13:07:54 -0500

    Finished Main Menu Function

--- CCP-3d-human-body-main/CCP-3d-human-body-main/HumanBodyEditor/Assets/Scripts/MainMenu.cs
+++ CCP-3d-human-body-main/CCP-3d-human-body-main/HumanBodyEditor/Assets/Scripts/MainMenu.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -6,8 +8,13 @@ public class MainMenu : MonoBehaviour
 {
     public void PlayGame ()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScenece().buildIndex + 1);
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
 
+    }
+    public void QuitGame ()
+    {
+        Debug.Log("QUIT!");
+        Application.Quit();
     }
     
 }
