c25ea3b64d8ad79e3061dee049b49d1ad7d3a838
salsaa-334 <salsabilhaque13@gmail.com>
2026-02-19 12:48:33 -0500

    Added function to MainMenu

--- CCP-3d-human-body-main/CCP-3d-human-body-main/HumanBodyEditor/Assets/Scripts/MainMenu.cs
+++ CCP-3d-human-body-main/CCP-3d-human-body-main/HumanBodyEditor/Assets/Scripts/MainMenu.cs
@@ -1,16 +1,13 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void PlayGame ()
     {
-        
-    }
+        SceneManager.LoadScene(SceneManager.GetActiveScenece().buildIndex + 1);
 
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
+    
 }
